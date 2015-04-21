f1425c9e2ac9e9ca8b4f49690e6dd619558958e0
roy <roy_radio@126.com>
2015-04-21 20:35:09 +0800

    first commit

---
+++ ch02/Greetings.groovy
@@ -0,0 +1,10 @@
+// Java code
+public class Greetings {
+    public static void main(String[] args) {
+        for(int i = 0; i < 3; i++) {
+            System.out.print("ho ");
+        }
+
+        System.out.println("Merry Groovy!");
+    }
+}
